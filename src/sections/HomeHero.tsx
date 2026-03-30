d8e26a1db8a6c254e0918c20b1bc54b73ddffae2
Keenan Satriatama <cvnvnasdf24@gmail.com>
2026-03-30 12:02:58 +0700

    perf: img loading optimization

--- src/sections/HomeHero.tsx
+++ src/sections/HomeHero.tsx
@@ -27,7 +27,6 @@ export default function HomeHero() {
             src={locr}
             alt="LOCR cropped"
             className="border-l border-r border-gray-500 px-2 py-0.5 h-full w-full object-cover"
-            loading="lazy"
           />
         </div>
       </div>
